ade215433e147a7f753363036eb517723e9b91fc
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-12-18 20:20:06 +0100

    analyser: Added automatic saving of figures

--- opxrd/analyser.py
+++ opxrd/analyser.py
@@ -36,13 +36,15 @@ class DatabaseAnalyser:
         self.plot_fourier(max_freq=2)
         self.plot_pca_scatter()
         self.plot_effective_components()
+        self.joined_db.show_histograms(save_fpath=os.path.join(self.output_dirpath, 'histogram.png'))
 
         self.show_label_fractions()
         self.print_total_counts()
 
     def plot_databases_in_single(self, n_patterns : int):
         for database in self.databases:
-            database.show_all(single_plot=True, limit_patterns=n_patterns)
+            save_fpath = os.path.join(self.output_dirpath, f'{database.name}_pattern_multiplot.png')
+            database.show_all(single_plot=True, limit_patterns=n_patterns, save_fpath=save_fpath)
 
     def plot_fourier(self, max_freq=2):
         for db in self.databases:
@@ -77,6 +79,7 @@ class DatabaseAnalyser:
             ft_ref_x, ft_ref_y = self.compute_fourier_transform(ref_x, ref_y, max_freq)
             plt.plot(ft_ref_x, ft_ref_y, alpha=0.75)
 
+            plt.savefig(os.path.join(self.output_dirpath, f'{db.name}_fourier.png'))
             plt.show()
 
     def plot_pca_scatter(self):
@@ -90,10 +93,53 @@ class DatabaseAnalyser:
         example_xy_list = [combined_pattern_list[idx].get_pattern_data() for idx in rand_indices]
         example_pca_coords =  [transformed_data[idx] for idx in rand_indices]
 
-        self._plot_pca_scatter(transformed_data, title=f'Two component scatter plot for combined Databases')
-        self._plot_pca_basis(pca, title=f'PCA basis')
-        self._plot_reconstructed(pca, example_xy_list, example_pca_coords, title=f'PCA-reconstructed patterns')
+        self._plot_pca_scatter(transformed_data, title=f'(1): Two component scatter plot for combined Databases')
+        self._plot_pca_basis(pca, title=f'(2): PCA basis')
+        self._plot_reconstructed(pca, example_xy_list, example_pca_coords, title=f'(3): Comparison of Original and Reconstructed Patterns for ')
         print('done')
+
+    def _plot_pca_scatter(self, transformed_data, title : str):
+        db_lens = [len(db.patterns) for db in self.databases]
+        max_points = 50
+        for j, l in enumerate(db_lens):
+            partial = transformed_data[:l]
+            if l > max_points:
+                indices = np.random.choice(len(partial), size=max_points, replace=False)
+                partial = partial[indices]
+            plt.scatter(partial[:, 0], partial[:, 1], label=f'db number {j}')
+            transformed_data = transformed_data[l:]
+
+        plt.title(f'{title}')
+        plt.xlabel('Component 1')
+        plt.ylabel('Component 2')
+        plt.legend()
+        plt.savefig(os.path.join(self.output_dirpath, f'pca_scatter_{title}.png'))
+        plt.show()
+
+    def _plot_pca_basis(self, pca, title : str):
+        b1, b2 = pca.inverse_transform(np.array([1,0])), pca.inverse_transform(np.array([0,1]))
+        x = np.linspace(start=0,stop=180, num=len(b1))
+        plt.plot(x, b1)
+        plt.plot(x, b2)
+        plt.title(f'{title}')
+        plt.savefig(os.path.join(self.output_dirpath, f'pca_basis_{title}.png'))
+        plt.show()
+
+    def _plot_reconstructed(self, pca, example_xy_list, example_pca_coords, title):
+        fig, axs = plt.subplots(len(example_xy_list), 2, figsize=(10, 5 * len(example_xy_list)))
+        fig.suptitle(f'{title}', fontsize=16)
+        for index, ((x1, y1), pca_coords) in enumerate(zip(example_xy_list, example_pca_coords)):
+            axs[index, 0].plot(x1, y1, 'b-')
+            self._set_ax_properties(axs[index, 0], title='Original pattern', xlabel='x', ylabel='Relative intensity')
+
+            reconstructed = pca.inverse_transform(pca_coords)
+            x = np.linspace(start=0, stop=180, num=len(reconstructed))
+            axs[index, 1].plot(x, reconstructed, 'r-')
+            self._set_ax_properties(axs[index, 1], title='Reconstructed pattern', xlabel='x', ylabel='Relative intensity')
+
+        plt.tight_layout()
+        plt.savefig(os.path.join(self.output_dirpath, f'pca_reconstructed_{title}.png'))
+        plt.show()
 
     def plot_effective_components(self):
         markers = ['o','s','^','v','D','p','*','+','x']
@@ -129,55 +175,12 @@ class DatabaseAnalyser:
         plt.xlabel(f'Fraction of maximum components')
         plt.ylabel(f'Average relative mismatch $\overline{{\Delta}}$')
         plt.legend(loc='upper right')
+        plt.savefig(os.path.join(self.output_dirpath, f'effective_components.png'))
 
         plt.show()
 
     # -----------------------
     # tools
-
-    def _plot_pca_scatter(self, transformed_data, title : str):
-        db_lens = [len(db.patterns) for db in self.databases]
-        max_points = 50
-        for j, l in enumerate(db_lens):
-            partial = transformed_data[:l]
-            if l > max_points:
-                indices = np.random.choice(len(partial), size=max_points, replace=False)
-                partial = partial[indices]
-            plt.scatter(partial[:, 0], partial[:, 1], label=f'db number {j}')
-            transformed_data = transformed_data[l:]
-
-        plt.title(f'(1): Two Component PCA Scatter Plot for {title}')
-        plt.xlabel('Component 1')
-        plt.ylabel('Component 2')
-        plt.legend()
-        plt.savefig(f'{self.output_dirpath}pca_scatter_{title}.png')
-        plt.show()
-
-    def _plot_pca_basis(self, pca, title : str):
-        b1, b2 = pca.inverse_transform(np.array([1,0])), pca.inverse_transform(np.array([0,1]))
-        x = np.linspace(start=0,stop=180, num=len(b1))
-        plt.plot(x, b1)
-        plt.plot(x, b2)
-        plt.title(f'(2): Principal Components for {title}')
-        plt.savefig(f'{self.output_dirpath}pca_basis_{title}.png')
-        plt.show()
-
-    def _plot_reconstructed(self, pca, example_xy_list, example_pca_coords, title):
-        fig, axs = plt.subplots(len(example_xy_list), 2, figsize=(10, 5 * len(example_xy_list)))
-        fig.suptitle(f'(3): Comparison of Original and Reconstructed Patterns for {title}', fontsize=16)
-        for index, ((x1, y1), pca_coords) in enumerate(zip(example_xy_list, example_pca_coords)):
-            axs[index, 0].plot(x1, y1, 'b-')
-            self._set_ax_properties(axs[index, 0], title='Original pattern', xlabel='x', ylabel='Relative intensity')
-
-            reconstructed = pca.inverse_transform(pca_coords)
-            x = np.linspace(start=0, stop=180, num=len(reconstructed))
-            axs[index, 1].plot(x, reconstructed, 'r-')
-            self._set_ax_properties(axs[index, 1], title='Reconstructed pattern', xlabel='x',
-                                    ylabel='Relative intensity')
-
-        plt.tight_layout()
-        plt.savefig(f'{self.output_dirpath}reconstructed_{title}.png')
-        plt.show()
 
     @staticmethod
     def _set_ax_properties(ax : Axes, title : str, xlabel : str, ylabel : str):
@@ -190,10 +193,10 @@ class DatabaseAnalyser:
 
     @staticmethod
     def compute_fourier_transform(x,y, max_freq : float):
-        N = len(y)  # Number of sample points
-        T = (x[-1] - x[0]) / (N - 1)  # Sample spacing
-        yf = np.fft.fft(y)  # Perform FFT
-        xf = np.fft.fftfreq(N, T)[:N // 2]  # Frequency axis
+        N = len(y)
+        T = (x[-1] - x[0]) / (N - 1)
+        yf = np.fft.fft(y)
+        xf = np.fft.fftfreq(N, T)[:N // 2]
 
         magnitude = 2.0 / N * np.abs(yf[:N // 2])
         valid_indices = xf <= max_freq
@@ -242,5 +245,3 @@ if __name__ == "__main__":
     full_dirpath = '/home/daniel/aimat/data/opXRD/final'
     opxrd_databases = OpXRD.as_database_list(root_dirpath=full_dirpath)
     analyser = DatabaseAnalyser(databases=opxrd_databases, output_dirpath='/tmp/opxrd_analysis')
-    analyser.plot_effective_components()
-    # analyser.run_all()
